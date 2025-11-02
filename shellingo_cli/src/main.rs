978d07c71da6e398529cba3ed5efaf318f31f8d6
tlvlp <peter@tlvlp.com>
2025-11-02 01:08:37 +0100

    Using the new and cleaner ratatui 0.30 api.

--- shellingo_cli/src/main.rs
+++ shellingo_cli/src/main.rs
@@ -1,44 +1,21 @@
-use crate::app::AppState;
-use ratatui::backend::CrosstermBackend;
-use ratatui::crossterm::execute;
-use ratatui::crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use ratatui::{Terminal, crossterm};
 use std::error::Error;
-use std::io;
+use crate::app::AppState;
 
 mod app;
 mod events;
 mod question_parser;
 mod ui;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    //Setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, crossterm::terminal::EnterAlternateScreen)?;
-
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    // App state
-    let mut app = AppState::new();
-
-    // Main loop
-    loop {
-        terminal.draw(|frame| ui::draw_ui(frame, &app))?;
-        if let Err(e) = events::handle_input(&mut app) {
-            eprintln!("{:?}", e);
-            break;
+fn main() -> std::io::Result<()> {
+    ratatui::run(|terminal| {
+        let mut app = AppState::new();
+        loop {
+            terminal.draw(|frame| ui::draw_ui(frame, &app))?;
+            if let Err(e) = events::handle_input(&mut app) {
+                eprintln!("{:?}", e);
+                break;
+            }
         }
-    }
-
-    // Cleanup
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        crossterm::terminal::LeaveAlternateScreen
-    )?;
-    terminal.show_cursor()?;
-
-    Ok(())
+        Ok(())
+    })
 }
